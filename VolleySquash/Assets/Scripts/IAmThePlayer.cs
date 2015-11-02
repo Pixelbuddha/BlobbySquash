e6869cef407f29a7cc7b00c4ee797c2f68f1c558
Sefisch <sefisch@googlemail.com>
2015-11-02 11:22:10 +0100

    Here we go

---
+++ VolleySquash/Assets/Scripts/IAmThePlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IAmThePlayer : MonoBehaviour
+{
+
+    private Vector3 velocity = new Vector3(0, 0, 0);
+    private float acceleration = 10;
+    private float maxSpeed = 10;
+
+
+    void MoveMe()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x -= acceleration * Time.fixedDeltaTime;
+        }
+        //================
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x += acceleration * Time.fixedDeltaTime;
+        }
+        transform.position += velocity * Time.fixedDeltaTime;
+        //================
+        velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
+    }
+    //================
+
+    void FixedUpdate()
+    {
+        MoveMe();
+    }
+}
