8544b71e6375e13e487551333f9b1cec35b6a245
Typhox <Typhox@Typhox.net>
2015-11-05 13:03:12 +0100

    more stuff

---
+++ VolleySquash/Assets/Scripts/Controls.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+	//Ingame Controls
+	public KeyCode Jump = KeyCode.Space;
+	public KeyCode Forward = KeyCode.A;
+	public KeyCode Backward = KeyCode.S;
+	public KeyCode Left = KeyCode.A;
+	public KeyCode Right = KeyCode.D;
+	public KeyCode Pause = KeyCode.Escape;
+
+	//If you are awesome, do a Dictionary for all the controls!
+}
